684b2ccff459b714ce1fa9e89fbea41e95d10dcf
abhishek.shreevats <abhishek.shreevats@0824d7e4-24a3-06a1-0a29-d7dfcbe1d914>
2010-08-01 22:12:42 +0000

    This is a raw basic crawler which extracts info from only the User class of the Facebook API. This code needs to be formatted at some places and cmdHelper.getOptionValue(<optionName>) does not work. Also, this code writes to a single file as of now.

--- Facebook/src/facebook/FacebookConnectionManager.java
+++ Facebook/src/facebook/FacebookConnectionManager.java
@@ -1,5 +1,8 @@
 package facebook;
 
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+
 /**
  * Created by IntelliJ IDEA.
  * User: shreevats
@@ -8,4 +11,22 @@ package facebook;
  * To change this template use File | Settings | File Templates.
  */
 public class FacebookConnectionManager {
+
+    /**
+     * an instance of facebook connection
+     */
+    private FacebookClient facebookClient;
+
+    public FacebookConnectionManager(String ACCESS_TOKEN){
+        facebookClient = new DefaultFacebookClient(ACCESS_TOKEN);
+    }
+
+    public FacebookClient getFacebookClient() {
+        return facebookClient;
+    }
+
+    public void setFacebookClient(FacebookClient facebookClient) {
+        this.facebookClient = facebookClient;
+    }
+
 }
